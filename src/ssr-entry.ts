063eec25fb16c1d781861c293f6dfb789f9f968d
pedroping <pedro.dias@moneyprestador.com.br>
2025-06-20 11:22:49 -0300

    Fix: add env port

--- src/ssr-entry.ts
+++ src/ssr-entry.ts
@@ -21,7 +21,7 @@ const hash = crypto
 
 const app = express();
 app.use(compression());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", async (_: Request, res: Response) => {
   const nonce = crypto.randomBytes(16).toString("base64");
